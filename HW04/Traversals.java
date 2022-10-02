86113405a885df56d22008bc3717f4c80c9217be
eastraining <21341281+eastraining@users.noreply.github.com>
2022-10-02 18:42:40 +0800

    Complete HW04

--- HW04/Traversals.java
+++ HW04/Traversals.java
@@ -27,6 +27,9 @@ public class Traversals<T extends Comparable<? super T>> {
    */
   public List<T> preorder(TreeNode<T> root) {
     // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    List<T> traversedList = new LinkedList<>();
+    recurPreOrder(root, traversedList);
+    return traversedList;
   }
 
   /**
@@ -44,6 +47,9 @@ public class Traversals<T extends Comparable<? super T>> {
    */
   public List<T> inorder(TreeNode<T> root) {
     // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    List<T> traversedList = new LinkedList<>();
+    recurInOrder(root, traversedList);
+    return traversedList;
   }
 
   /**
@@ -61,6 +67,66 @@ public class Traversals<T extends Comparable<? super T>> {
    */
   public List<T> postorder(TreeNode<T> root) {
     // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    List<T> traversedList = new LinkedList<>();
+    recurPostOrder(root, traversedList);
+    return traversedList;
+  }
 
+  private void recurPreOrder(TreeNode<T> node, List<T> traversedList) {
+    if (node != null) {
+      traversedList.add(node.getData());
+      recurPreOrder(node.getLeft(), traversedList);
+      recurPreOrder(node.getRight(), traversedList);
+    }
+  }
+
+  private void recurInOrder(TreeNode<T> node, List<T> traversedList) {
+    if (node != null) {
+      recurInOrder(node.getLeft(), traversedList);
+      traversedList.add(node.getData());
+      recurInOrder(node.getRight(), traversedList);
+    }
+  }
+
+  private void recurPostOrder(TreeNode<T> node, List<T> traversedList) {
+    if (node != null) {
+      recurPostOrder(node.getLeft(), traversedList);
+      recurPostOrder(node.getRight(), traversedList);
+      traversedList.add(node.getData());
+    }
+  }
+
+  // Testing
+  public static void main(String[] args) {
+    int[] testNums = { 50, 25, 100, 10, 75, 125, 110 };
+    TreeNode<Integer> testRoot = new TreeNode<>(50);
+    testRoot.setLeft(new TreeNode<Integer>(25));
+    testRoot.setRight(new TreeNode<Integer>(100));
+    testRoot.getLeft().setLeft(new TreeNode<Integer>(10));
+    testRoot.getRight().setLeft(new TreeNode<Integer>(75));
+    testRoot.getRight().setRight(new TreeNode<Integer>(125));
+    testRoot.getRight().getRight().setLeft(new TreeNode<Integer>(110));
+
+    Traversals<Integer> testTraversal = new Traversals<>();
+    List<Integer> results = testTraversal.preorder(testRoot);
+    System.out.print("Preorder traversal:");
+    for (Integer result : results) {
+      System.out.print(" " + result);
+    }
+    System.out.println();
+
+    results = testTraversal.inorder(testRoot);
+    System.out.print("Inorder traversal:");
+    for (Integer result : results) {
+      System.out.print(" " + result);
+    }
+    System.out.println();
+
+    results = testTraversal.postorder(testRoot);
+    System.out.print("Postorder traversal:");
+    for (Integer result : results) {
+      System.out.print(" " + result);
+    }
+    System.out.println();
   }
 }
