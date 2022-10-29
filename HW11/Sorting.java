7dd342e4f80c0a704c4a2859957ead0abd569daa
eastraining <21341281+eastraining@users.noreply.github.com>
2022-10-29 22:07:55 +0800

    Complete HW11

--- HW11/Sorting.java
+++ HW11/Sorting.java
@@ -37,7 +37,45 @@ public class Sorting {
    * @param arr        The array to be sorted.
    * @param comparator The Comparator used to compare the data in arr.
    */
-  public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {}
+  public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {
+    if (arr.length == 1) {
+      return;
+    } else {
+      int mid = arr.length / 2;
+      T[] leftArr = (T[]) new Object[mid];
+      T[] rightArr = (T[]) new Object[arr.length - mid];
+      for (int i = 0; i < mid; i++) {
+        leftArr[i] = arr[i];
+        rightArr[i] = arr[i + mid];
+      }
+      if (arr.length % 2 > 0) rightArr[mid] = arr[arr.length - 1];
+
+      mergeSort(leftArr, comparator);
+      mergeSort(rightArr, comparator);
+
+      int leftIdx = 0, rightIdx = 0, currentIdx = 0;
+      while (leftIdx < mid && rightIdx < arr.length - mid) {
+        if (comparator.compare(leftArr[leftIdx], rightArr[rightIdx]) <= 0) {
+          arr[currentIdx] = leftArr[leftIdx];
+          leftIdx++;
+        } else {
+          arr[currentIdx] = rightArr[rightIdx];
+          rightIdx++;
+        }
+        currentIdx++;
+      }
+      while (leftIdx < mid) {
+        arr[currentIdx] = leftArr[leftIdx];
+        leftIdx++;
+        currentIdx++;
+      }
+      while (rightIdx < arr.length - mid) {
+        arr[currentIdx] = rightArr[rightIdx];
+        rightIdx++;
+        currentIdx++;
+      }
+    }
+  }
 
   /**
    * Implement LSD (least significant digit) radix sort.
@@ -69,5 +107,104 @@ public class Sorting {
    *
    * @param arr The array to be sorted.
    */
-  public static void lsdRadixSort(int[] arr) {}
+  public static void lsdRadixSort(int[] arr) {
+    int maxNum = 0;
+    int passes = 0;
+    for (int item : arr) {
+      if (Math.abs(item) < 0) {
+        maxNum = -1;
+        passes = 10;
+        break;
+      } else if (Math.abs(item) > maxNum) {
+        maxNum = Math.abs(item);
+      }
+    }
+    if (maxNum != -1) {
+      while (maxNum > 0) {
+        maxNum = maxNum / 10;
+        passes++;
+      }
+    }
+    LinkedList<Integer>[] buckets = new LinkedList[19];
+    for (int i = 0; i < passes; i++) {
+      int divisor = i == 0 ? 1 : i * 10;
+      for (int j = 0; j < arr.length; j++) {
+        int current = ((arr[j] / divisor) % 10) + 9;
+        if (buckets[current] == null) buckets[current] =
+          new LinkedList<Integer>();
+        buckets[current].addLast(arr[j]);
+      }
+      int arrIdx = 0;
+      for (int j = 0; j < buckets.length; j++) {
+        if (buckets[j] != null) {
+          while (!buckets[j].isEmpty()) {
+            arr[arrIdx] = buckets[j].pollFirst();
+            arrIdx++;
+          }
+        }
+      }
+    }
+  }
+
+  // Tests
+  private static <T> void printArr(T[] arr) {
+    System.out.print("Array contains: ");
+    String result = "";
+    for (T item : arr) {
+      System.out.print(item + " ");
+    }
+    System.out.println();
+  }
+
+  private static void printIntArr(int[] arr) {
+    System.out.print("Array contains: ");
+    String result = "";
+    for (int item : arr) {
+      System.out.print(item + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    Integer[] intTest = {
+      3,
+      100,
+      25,
+      45,
+      68,
+      -23,
+      79,
+      31,
+      40,
+      55,
+      45,
+      Integer.MIN_VALUE,
+      Integer.MAX_VALUE,
+    };
+    Comparator<Integer> intComp = (Integer a, Integer b) -> {
+      if (a == Integer.MIN_VALUE) return -1;
+      if (b == Integer.MIN_VALUE) return 1;
+      return a - b;
+    };
+    mergeSort(intTest, intComp);
+    printArr(intTest);
+
+    int[] intTest2 = {
+      3,
+      100,
+      25,
+      45,
+      68,
+      -23,
+      79,
+      31,
+      40,
+      55,
+      45,
+      Integer.MIN_VALUE,
+      Integer.MAX_VALUE,
+    };
+    lsdRadixSort(intTest2);
+    printIntArr(intTest2);
+  }
 }
